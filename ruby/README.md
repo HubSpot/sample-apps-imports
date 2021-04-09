6d59232a1aa2b160fb3b2a301624087fbc6afb02
Павел Лавринович <plaurynovich@hubspot.com>
2021-04-09 19:19:50 +0300

    Ruby imports

---
+++ ruby/README.md
@@ -0,0 +1,52 @@
+# HubSpot-ruby CRM-imports sample app
+
+### Requirements
+
+1. ruby 2.6.3
+2. [Configured](https://github.com/HubSpot/sample-apps-manage-crm-imports/blob/main/README.md#how-to-run-locally) .env file
+
+### Running
+
+1. Install dependencies
+
+```
+bundle install
+```
+
+2. Commands
+
+Show all commands (get help)
+
+```
+ruby cli.rb -h
+```
+
+Get imports
+
+```
+ruby cli.rb -m get_page
+```
+
+Get an import by id
+
+```
+ruby cli.rb -m get_by_id -i [import_id]
+```
+
+Create an import
+
+```
+ruby cli.rb -m create -f [filename] -c [columns_config]
+```
+
+Example:
+
+```
+ruby cli.rb -m create -f import_emails.csv -c '[{"columnName": "First Name", "propertyName": "firstname", "columnObjectType": "CONTACT"}, {"columnName": "Email", "propertyName": "email", "columnObjectType": "CONTACT" }]'
+```
+
+Cancel an import
+
+```bash
+ruby cli.rb -m cancel -i [import_id]
+```
---
+++ ruby/cli.rb
@@ -0,0 +1,78 @@
+require_relative 'config'
+require 'optparse'
+require 'ostruct'
+require 'json'
+
+class Cli
+  REQUIRED_PARAMS = {
+    cancel: %i(import_id),
+    create: %i(),
+    get_by_id: %i(import_id),
+    get_page: %i(),
+  }.freeze
+
+  def initialize(options)
+    @options = options
+    @method = options[:method]&.to_sym
+  end
+
+  def run
+    validate
+    call_api
+  end
+
+  private
+
+  attr_reader :options, :method
+
+  def validate
+    raise(ArgumentError, 'Please, provide method to call with -m or --method') unless method
+    missing_params = REQUIRED_PARAMS[method] - options.to_h.keys
+    raise(ArgumentError,"Please, provide missing params #{missing_params} for the method. Use help (-h) if you need.")if missing_params.any?
+  end
+
+  def call_api
+    api = Hubspot::Crm::Imports::CoreApi.new
+    api.public_send(method, *params)
+  end
+
+  def params
+    required_params = REQUIRED_PARAMS[method]
+    mapped_params = required_params.map { |param| options[param] }
+    opts = options[:opts] || {}
+    opts[:auth_names] = 'hapikey'
+    opts.merge!(import_request: import_request, files: file) if method == :create
+    mapped_params << opts
+    mapped_params
+  end
+
+  def import_request
+    {
+      "name": "test_import",
+      "files": [
+        {
+          "fileName": options[:csv_filename],
+          "fileImportPage": {
+            "hasHeader": true,
+            "columnMappings": options[:column_mapping]
+          }
+        }
+      ]
+    }.to_json
+  end
+
+  def file
+    File.open(options[:csv_filename])
+  end
+end
+
+options = OpenStruct.new
+OptionParser.new do |opt|
+  opt.on('-m', '--method METHOD', 'Method to run') { |o| options.method = o }
+  opt.on('-i', '--id ID', 'The id of import') { |o| options.import_id = o }
+  opt.on('-f', '--file FILE', 'CSV filename') { |o| options.csv_filename = o }
+  opt.on('-c', '--column COLUMN', 'Column mapping config') { |o| options.column_mapping = JSON.parse(o) }
+  opt.on('-o', '--options OPTIONS', 'Options to pass') { |o| options.opts = JSON.parse(o) }
+end.parse!
+
+p Cli.new(options).run
